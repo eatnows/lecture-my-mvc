ee0afdaa0f2f7df30411a8c50f51f74ae939f70e
eatnows <gagyasindev@gmail.com>
2023-04-17 22:50:46 +0900

    feat: 요청 시 새로운 Thread 를 생성하여 처리

--- src/main/java/org/example/web_calculator/CustomWebApplicationServer.java
+++ src/main/java/org/example/web_calculator/CustomWebApplicationServer.java
@@ -29,30 +29,13 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected.");
 
                 /**
-                 * Step1 - 사용자 요청을 메인 Thread 가 처리하도록 한다.
+                 * Step2 - 사용자 요청이 들어올 때마다 Thread 를 새로 생성해서 사용자 요청을 처리하도록 한다.
                  */
-
-                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryString();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-                }
+                // Thread 를 새로 생성하면 메모리를 새로 할당받는다.
+                // Thread 가 많아지면 CPU 컨텍스트 스위칭이 많아진다.
+                // CPU 의 사용량와 메모리의 사용량이 증가된다.
+                // ----> 서버 다운
+                new Thread(new ClientRequestHandler(clientSocket)).start();
             }
         }
     }
